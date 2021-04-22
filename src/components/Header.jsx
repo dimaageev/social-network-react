d82cae5ff904aa17afc19bbd63ffc9dd51c65e1c
Dima <dimitri1ageev@gmail.com>
2021-04-22 23:06:03 +0400

    separated-on-components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://media.sproutsocial.com/uploads/2017/08/Social-Media-Video-Specs-Feature-Image.png" />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>NewPost</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
